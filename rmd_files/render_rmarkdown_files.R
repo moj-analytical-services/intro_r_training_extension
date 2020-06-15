e28af7da315aad259d8a22718fbede7d82ce2156
chloe.e.pugh91@gmail.com <chloe.e.pugh91@gmail.com>
2020-06-15 16:39:53 +0000

    Add placeholder for appendix

--- rmd_files/render_rmarkdown_files.R
+++ rmd_files/render_rmarkdown_files.R
@@ -10,3 +10,7 @@ rmarkdown::render("rmd_files/slides.Rmd", output_format = "ioslides_presentation
 # move rendered files to main directory
 file.rename("rmd_files/README.md", "README.md")
 file.rename("rmd_files/slides.html", "slides.html")
+
+# extract R code from content.Rmd into separate script
+# knitr::purl("rmd_files/README.Rmd")
+# file.rename("content.R", "example_code.R")
