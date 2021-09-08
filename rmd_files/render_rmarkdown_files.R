325284559c05c87c6b9541aebeaf1c72fc9ccb24
chloe.e.pugh91@gmail.com <chloe.e.pugh91@gmail.com>
2021-09-08 18:29:05 +0000

    markdown code block fixes

--- rmd_files/render_rmarkdown_files.R
+++ rmd_files/render_rmarkdown_files.R
@@ -5,11 +5,11 @@ rmarkdown::render("rmd_files/README.Rmd", output_format = "github_document")
 file.remove("rmd_files/README.html")
 
 # render slides.Rmd as an ioslides presentation
-rmarkdown::render("rmd_files/slides.Rmd", output_format = "ioslides_presentation")
+# rmarkdown::render("rmd_files/slides.Rmd", output_format = "ioslides_presentation")
 
 # move rendered files to main directory
 file.rename("rmd_files/README.md", "README.md")
-file.rename("rmd_files/slides.html", "slides.html")
+# file.rename("rmd_files/slides.html", "slides.html")
 
 # extract R code from content.Rmd into separate script
 # knitr::purl("rmd_files/README.Rmd")
